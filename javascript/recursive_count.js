6d9576a74ce03ec5c15f3f2f6ce14e08425c20dd
Walker Le <walker.le88@gmail.com>
2023-06-13 19:11:11 -0600

    javascript and ruby complete

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,9 +1,27 @@
 function recursiveCount(num = 0) {
   // type your code here
+  // let count = 0;
+
+  // while(count < 10) {
+  //   console.log(count);
+  //   ++count;
+  // }
+  if(num >= 10) {
+    // console.log(num);
+  } else if(num < 0) {
+    console.log('less than 0');
+  } else {
+    console.log(num);
+    recursiveCount(num + 1);
+    // recursiveCount(++num);
+    // ++num increments then invokes the function/loop
+    // num++ invokes the function/loop then increments
+  }
 }
 
 if (require.main === module) {
   recursiveCount();
+  // recursiveCount(10);
 }
 
 module.exports = recursiveCount;
--- ruby/recursive_count.rb
+++ ruby/recursive_count.rb
@@ -1,5 +1,16 @@
 def recursive_count(num = 0)
   # type your code in here
+  if num === 10
+    return
+  elsif num <= 10
+    puts num
+    recursive_count(num + 1)
+  end
+
+  # ALTERNATE SOLUTION
+  # return if num >= 10
+  # puts num
+  # recursive_count(num + 1)
 end
 
 if __FILE__ == $PROGRAM_NAME
